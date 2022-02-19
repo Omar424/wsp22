581edd5eab425c8d77601e865b9afcef24d6b6bc
Omar Mustafa <omar8mustafa@gmail.com>
2022-02-19 16:11:14 +0100

    update

--- app.rb
+++ app.rb
@@ -24,7 +24,15 @@ get('/login') do
 end
 
 get('/inventory') do
-    return slim(:inventory)
+
+    if session["user_id"] != nil
+        connect_to_db(path)
+        inventory = db.execute("SELECT * FROM todos WHERE user_id = ?", [session["user_id"]])
+        slim(:"inventory", locals: { inventory: inventory })
+    else
+        redirect("/")
+    end
+
 end
 
 get('/webshop') do
@@ -34,6 +42,9 @@ get('/webshop') do
     double_array = lines.map do |element|
         element.split(",")
     end
+
+    connect_to_db(path)
+    card = db.execute("SELECT * FROM cards"])
     
     array_with_hashes = double_array.map do |element| {
         name:element[0],
@@ -45,21 +56,50 @@ get('/webshop') do
     }
     end
 
+    # return slim(:webshop, locals:{card: card})
     return slim(:webshop, locals:{info:array_with_hashes})
 end
 
 post('/register') do
+    username = params["username"]
+    password = params["password"]
+    password_confirmation = params["password_confirmation"]
     connect_to_db('db/db.db')
-    regusername = params["reg_username"]
-    regpassword = BCrypt::Password.create(params["reg_password"])
-    redirect('/login')
+    check_user = db.execute("SELECT * FROM users WHERE username = ?", [username])
+    
+    #Registeringskontroll
+    if check_user.empty?
+        if password == password_confirmation
+          hashed_password = BCrypt::Password.create(password)
+          db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashed_password])
+          return slim(:login, locals: { error: "", sucess: "Logga in för att se dina todos" })
+        else
+          return slim(:register, locals: { error: "Passwords does not match" })
+        end
+    else
+        return slim(:register, locals: { error: "Username already exists" })
+    end
 end
 
 post('/login') do
-    connect_to_db('db/db.db')
-    loginuname = params["log_username"]
-    loginpword = params["log_password"]
-    redirect('/teams')
+    username = params["log_username"]
+    password = params["log_password"]
+    connect_to_db('path')
+    user = db.execute("SELECT * FROM users WHERE username = ?", [username])
+
+    #Användarhantering med inloggning
+    if user.empty?
+        # Fel hantering, visar att användaren inte finns!
+        return slim(:login, locals: { error: "Användaren finns inte!", sucess: "" })   
+    elsif BCrypt::Password.new(user[0]["password"]) == password
+        session[:user_id] = user[0]["id"]
+        puts "User id: #{session[:user_id]}"
+        redirect('/teams')
+    else
+        # Fel hantering, visar att lösenordet inte matchar!
+        return slim(:login, locals: { error: "Fel lössenord!", sucess: "" })
+    end
+
 end
 
 post('/buy') do

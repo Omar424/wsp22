a47656736efb09410c8cd82281280dc629f242d4
Omar Mustafa <omar8mustafa@gmail.com>
2022-03-29 12:11:49 +0200

    update

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ enable :sessions
 
 #get routes
 
-
+#Första sidan
 get('/') do
     if session["user_id"] != nil
         redirect('/webshop')
@@ -17,57 +17,50 @@ get('/') do
     end
 end
 
+#Registrering
 get('/register') do
     return slim(:"user/register", locals: { error: "" })
 end
 
+#Logga in
 get('/login') do
     slim(:"user/login", locals: { error: "", sucess: "" })
 end
 
-get('/logout') do
-    session.destroy
-    redirect("/")
-end
-
+#Error
 get('/error') do
     slim(:error)
 end
 
-post('/cards/:id/delete') do
-    card_id = params["id"].to_i
-    delete_card(card_id)
-end
-
+#Route för att komma till användarens eller andras profil
 get('/user/:id/profile') do
+    user_id = params[:id]
 
-    # if session["user_id"] != nil
+    if session["user_id"] != nil
         db = connect_to_db('db/db.db')
-        user_id = params[:id]
         user_data = db.execute("SELECT * FROM users where id = ?", user_id).first
         user_cards = db.execute("SELECT * FROM cards where user_id = ?", user_id)
-
-        p user_data
-        p user_cards
-        
+    
         if user_data == nil
-            slim(:error, locals:{message: "user with id: #{user_id} does not exist"})
+            "user with id: #{user_id} does not exist"
         elsif user_cards.length == 0
             slim(:"user/index", locals:{user_info:user_data, cards:user_cards, message: "user with id #{user_id} hasn't created a card"})
         else
             slim(:"user/index", locals:{user_info:user_data, cards:user_cards, message: "temp"})
         end
-    # else
-        # redirect('/')
-    # end
+    else
+        route = "/user/#{user_id}/profile"
+        slim(:"/error", locals:{m:"Du måste vara inloggad för att se routen #{route}"})
+    end
 end
 
 get('/uploaded_pictures/:type/:name') do
     File.read("uploaded_pictures/#{params[:type]}/#{params[:name]}")
 end
 
+#Inventory, användarens kort
 get('/inventory') do
-
+    
     if session["user_id"] != nil
         db = connect_to_db("db/db.db")
         cards = db.execute("SELECT * FROM cards WHERE user_id = ?", [session["user_id"]])
@@ -76,46 +69,59 @@ get('/inventory') do
     else
         redirect('/')
     end
-
-    # get_user_cards()
-
+    
+    # inventory()
 end
 
+#Webshop
 get('/webshop') do
     get_all_cards()
 end
 
+#Visa 1 kort
 get('/card/:id') do
-    db = connect_to_db("db/db.db")
     card_id = params[:id].to_i
 
-    card_data = db.execute("SELECT * FROM cards WHERE id = ?", card_id).first
-    # p card_data
-    # p card_data["rating"]
+    if session["user_id"] != nil
+        db = connect_to_db("db/db.db")
+        card_data = db.execute("SELECT * FROM cards WHERE id = ?", card_id).first
 
-    if card_data != nil
-        slim(:"cards/show", locals:{card:card_data, id:card_id})
+        if card_data != nil
+            slim(:"cards/show", locals:{card:card_data, id:card_id})
+        else
+            "Card with id: #{card_id} does not exist :/"
+        end
     else
-        message = "Card with id: #{card_id} does not exist :/"
-        slim(:"error", locals:{message:message})
+        route = "/cards/#{card_id}"
+        "Du måste logga in för att komma till routen #{route}"
     end
-
 end
 
-#Man ska inte komma till denna routen utan att vara inloggad, session_id ska vara tillgängligt
+#Skapa kort
 get('/cards/new') do
-    slim(:"cards/new", locals:{session_id: session["user_id"]})
+    if session["user_id"] != nil
+        slim(:"cards/new", locals:{session_id: session["user_id"]})
+    else
+        route = "/cards/new"
+        "Du måste logga in för att komma till routen #{route}"
+    end
 end
 
+#Uppdatera kort
 get('/cards/:id/edit') do
-    id = params[:id].to_i
-    db = connect_to_db('db/db.db')
-    card = db.execute("SELECT * FROM cards WHERE id = ?", id).first
-    slim(:"/cards/edit", locals:{card:card})
+    if session[:user_id] != nil
+        id = params[:id].to_i
+        db = connect_to_db('db/db.db')
+        card = db.execute("SELECT * FROM cards WHERE id = ?", id).first
+        slim(:"/cards/edit", locals:{card:card})
+    else
+        "Du måste logga in för att redigera ett kort"
+    end
 end
 
-#post routes
+#post-routes
 
+#Registrering
 post('/register') do
     username = params["username"]
     password = params["password"]
@@ -123,61 +129,52 @@ post('/register') do
     register_user(username, password, password_conf)
 end
 
+#Logga in
 post('/login') do
     username = params["username"]
     password = params["password"]
     login_user(username, password)
 end
 
-#Man ska inte komma till denna routen utan att vara inloggad, session_id ska vara tillgängligt
+#Skapa kort
 post('/create_card') do
-
     name = params[:name]
     position = params[:position]
     rating = params[:rating]
     stat1 = params[:stat1]
     stat2 = params[:stat2]
     stat3 = params[:stat3]
-    p session[:user_id]
     # file_path för ruby att veta vart den ska skriva in filen
     p file_face_path = "public/uploaded_pictures/faces/#{params[:player_face][:filename]}"
     p file_club_path = "public/uploaded_pictures/clubs/#{params[:club][:filename]}"
     # path för get_routen att veta source för bilden
     p face_path = "uploaded_pictures/faces/#{params[:player_face][:filename]}"
     p club_path = "uploaded_pictures/clubs/#{params[:club][:filename]}"
-
-    File.open(file_face_path, "wb") do |f|
-        f.write(params[:player_face][:tempfile].read)
-    end
-
-    File.open(file_club_path, "wb") do |f|
-        f.write(params[:club][:tempfile].read)
-    end
-
     create_card(name, position, club_path, face_path, rating, stat1, stat2, stat3, session[:user_id])
 end
 
+#Köpa kort
+post('/cards/:id/buy') do
+    card_id = params["id"].to_i
+    buy_card(card_id)
+end
+
+#Uppdatera kort
 post('/cards/:id/update') do
-    id = params[:id].to_i
+    card_id = params[:id].to_i
     name = params[:name]
     rating = params[:rating].to_i
     position = params[:position]
-    # card_id = params[:card_id].to_i
-    db = SQLite3::Database.new("db/db.db")
-    db.execute("UPDATE cards SET name = ?, rating = ?, position = ? WHERE id = ?", name, rating, position, id)
-    redirect('/webshop')
+    update_card(card_id, name, rating, position)
 end
 
-post('/buy') do
-    add_to_inventory(card_data)
-    # update ägaren av kortet, nya ägaren den som köpte
-    redirect('/webshop')
+#Radera kort
+post('/cards/:id/delete') do
+    card_id = params["id"].to_i
+    delete_card(card_id)
 end
 
-post('/delete') do
-    delete_card()
-end
-
+#Logga ut
 post('/logout') do
     session.destroy
     redirect('/')
--- model.rb
+++ model.rb
@@ -1,11 +1,13 @@
 require_relative './app.rb'
 
+#Anslutning till databas
 def connect_to_db(path)
     db = SQLite3::Database.new(path)
     db.results_as_hash = true
     return db
 end
 
+#Registrering
 def register_user(username, password, password_conf)
     db = connect_to_db('db/db.db')
     user = db.execute("SELECT * FROM users WHERE username = ?", username).first
@@ -24,6 +26,7 @@ def register_user(username, password, password_conf)
     end
 end
 
+#Logga in
 def login_user(username, password)
     db = connect_to_db('db/db.db')
     user = db.execute("SELECT * FROM users WHERE username = ?", [username]).first
@@ -41,25 +44,41 @@ def login_user(username, password)
     end
 end
 
+#Funktion för att skapa kort
 def create_card(name, position, club, face, rating, stat1, stat2, stat3, user_id)
     db = connect_to_db('db/db.db')
     db.execute("INSERT INTO cards (name, position, club, image, rating, user_id) VALUES (?,?,?,?,?,?)", [name, position, club, face, rating, user_id])
     db.execute("INSERT INTO stats (stat1, stat2, stat3) VALUES (?,?,?)", [stat1, stat2, stat3])
-    redirect('/webshop', locals:{klart: "kort skapat, den finns nu i webbshoppen"})
+    File.open(file_face_path, "wb") do |f|
+        f.write(params[:player_face][:tempfile].read)
+    end
+    File.open(file_club_path, "wb") do |f|
+        f.write(params[:club][:tempfile].read)
+    end
+    redirect('/webshop')
 end
 
-def get_user_cards()
-    db = connect_to_db('db/db.db')
-    user_cards = db.execute("SELECT * FROM cards where user_id = ?", session[:id])
-    user_cards_stats = db.execute("SELECT stat1, stat2, stat3 FROM stats where user_id = ?", session[:id])
-    return slim(:webshop, locals:{cards:user_cards, stats:user_cards_stats})
+#Funktion för att få användarens kort, inventory
+# def get_user_cards()
+#     db = connect_to_db('db/db.db')
+#     user_cards = db.execute("SELECT * FROM cards where user_id = ?", session[:id])
+#     user_cards_stats = db.execute("SELECT stat1, stat2, stat3 FROM stats where user_id = ?", session[:id])
+#     return slim(:webshop, locals:{cards:user_cards, stats:user_cards_stats})
+# end
+
+#Funktion för att få ägaren av ett kort
+def get_card_owner(id)
+    db = connect_to_db("db/db.db")
+    owner = db.execute("SELECT username FROM users WHERE id = ?", id)
+    slim(:"webshop", locals:{owner:owner})
 end
 
+#Funktion för att få alla kort i databasen, webshop
 def get_all_cards()
-    
     if session["user_id"] != nil
         db = connect_to_db('db/db.db')
         cards = db.execute("SELECT * FROM cards")
+        users = db.execute("SELECT * FROM users")
         # stats = db.execute("SELECT stat1, stat2, stat3 FROM stats")
 
         # stat_1_array = []
@@ -74,23 +93,32 @@ def get_all_cards()
         #     # stats:stats, stat1:stat_1_array, stat2:stat_2_array, stat3:stat_3_array
         #     i += 1
         # end
-
-        return slim(:webshop, locals:{cards:cards})
+        return slim(:webshop, locals:{cards:cards, users:users})
     else
-        redirect("/")
+        redirect('/')
     end
 end
 
-def add_to_inventory()
-    #kod
+#Funktion för att köpa kort
+def buy_card(card_id)
     db = connect_to_db('db/db.db')
-    db.execute("UPDATE VALUES in cards user_id to user_id")
+    db.execute("UPDATE cards SET user_id = ? WHERE id = ?", session[:user_id], card_id)
+    redirect('/webshop')
 end
 
+#Funktion för att uppdatera kort
+def update_card(card_id, name, rating, position)
+    db = connect_to_db("db/db.db")
+    db.execute("UPDATE cards SET name = ?, rating = ?, position = ? WHERE id = ?", name, rating, position, card_id)
+    redirect('/webshop')
+end
+
+#Funktion för att ta bort kort
 def delete_card(card_id)
     db = connect_to_db('db/db.db')
     db.execute("DELETE FROM cards where id = ?", card_id)
     redirect('/webshop')
+    # location.reload()
 end
 
 # def temp()
